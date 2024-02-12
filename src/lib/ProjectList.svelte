5653146e8dd3ccb9d705af0ecc80938e30cad9a7
ROSERAT Ugo <roserat.ugo@gmail.com>
2024-02-12 12:54:51 +0100

    use tailwind

--- src/lib/ProjectList.svelte
+++ src/lib/ProjectList.svelte
@@ -1,84 +1,60 @@
 <script>
     import { onMount } from 'svelte';
-    let studentProjects = []; // Initialiser comme tableau vide
+    let studentProjects = [];
+    let projectOfTheMonth = {};
 
     onMount(async () => {
         const response = await fetch('https://bdeapi.ugoroserat.com/projects');
         if (response.ok) {
             let projects = await response.json();
-            // Ajuster les URLs des images pour chaque projet
             studentProjects = projects.map(project => {
                 if (project.image && !project.image.startsWith('http')) {
-                    // Ajouter le préfixe seulement si l'URL de l'image n'est pas absolue
                     project.image = `https://github.com${project.image.startsWith('/') ? '' : '/'}${project.image}`;
                 }
                 return project;
             });
+            projectOfTheMonth = projects.find(p => p.id === 2);
         } else {
             console.error('Erreur lors de la récupération des projets', response.statusText);
         }
     });
 </script>
 
-<main>
-    <div class="projects-container">
-        {#each studentProjects as project}
-            <a href={project.url} target="_blank" rel="noopener noreferrer" class="project-box-link">
-                <div class="project-box">
-                    <img src={project.image} alt={`Image de ${project.title}`} class="project-image"/>
-                    <h3>{project.title.toUpperCase()}</h3>
-                    <p>{project.description}</p>
-                    <span class="project-name">{project.name}</span>
+<main class="bg-gray-900 text-white">
+    <div class="navbar bg-gray-800 p-5 flex items-center">
+        <img src="/logo.svg" alt="" class="h-15">
+    </div>
+    <div class="projects-container p-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <!-- Projet du mois -->
+            <div class="lg:col-span-1 lg:row-span-2 bg-gray-700 shadow-lg rounded-lg overflow-hidden flex flex-col">
+                <div class="text-center py-2 bg-gray-600 text-white font-bold uppercase">Projet du mois</div>
+                <img src={projectOfTheMonth.image} alt={`Image de ${projectOfTheMonth.title}`} class="w-full object-cover" style="height: 300px;"/>
+                <div class="p-4 flex-grow">
+                    <h3 class="text-lg font-semibold">{projectOfTheMonth.title}</h3>
+                    <p>{projectOfTheMonth.description}</p>
                 </div>
-            </a>
-        {/each}
+                <div class="p-4 bg-gray-800 flex justify-between items-center">
+                    <p class="text-sm">{projectOfTheMonth.name}<br>{projectOfTheMonth.classname}</p>
+                    <a href={projectOfTheMonth.url} target="_blank"
+                       class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-300">Découvrir</a>
+                </div>
+            </div>
+            <!-- Autres projets -->
+            {#each studentProjects as project}
+                <div class="project-box bg-gray-700 shadow-lg rounded-lg overflow-hidden">
+                    <img src={project.image} alt={`Image de ${project.title}`} class="w-full"/>
+                    <div class="p-4">
+                        <h3 class="text-lg font-semibold">{project.title}</h3>
+                        <p>{project.description}</p>
+                    </div>
+                    <div class="p-4 bg-gray-800 flex justify-between items-center">
+                        <p class="text-sm">{project.name}<br>{project.classname}</p>
+                        <a href={project.url} target="_blank"
+                           class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-300">Découvrir</a>
+                    </div>
+                </div>
+            {/each}
+        </div>
     </div>
 </main>
-
-<style>
-    .projects-container {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 20px;
-        justify-content: center;
-        padding: 20px;
-    }
-
-    .project-box-link {
-        text-decoration: none; /* Enlève le soulignement des liens */
-        color: inherit; /* Hérite la couleur du texte de l'élément parent */
-        width: 300px; /* Assure que le lien ait la même largeur que la boîte */
-    }
-
-    .project-box {
-        border: 2px solid #646cff;
-        border-radius: 10px;
-        padding: 20px;
-        text-align: center;
-        transition: transform 0.3s ease;
-        overflow: hidden;
-    }
-
-    .project-box:hover {
-        transform: translateY(-5px);
-        border-color: #ff3e00;
-    }
-
-    .project-image {
-        width: 100%;
-        height: auto;
-        border-radius: 10px;
-        margin-bottom: 10px;
-    }
-
-    h3 {
-        text-transform: uppercase; /* Met le titre en majuscules */
-        margin: 0 0 10px 0; /* Ajuste l'espacement autour du titre */
-    }
-
-    .project-name {
-        display: block; /* Rend le nom du projet un bloc pour un meilleur espacement */
-        font-weight: bold;
-        margin-top: 10px;
-    }
-</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.svelte",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
